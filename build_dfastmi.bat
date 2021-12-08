f66912e1a06217c423906ccf89308a0e5677d4de
Bert Jagers <hrajagers@gmail.com>
2021-12-08 21:02:46 +0200

    Update __main__ and build files to make sure that the new code also runs in compile mode. Supporting a Shape file as line introduces a large number of dependencies and complexities which are similar to those experienced with D-FAST Bank Erosion last year; hence most of the adjustments implemented now have been copied from dfastbe.

--- build_dfastmi.bat
+++ build_dfastmi.bat
@@ -9,5 +9,30 @@ copy dfastmi\*.png dfastmi.dist\dfastmi
 rem Unable to load file 'Dutch_rivers.ini'
 copy dfastmi\Dutch_rivers.ini dfastmi.dist\dfastmi
 
+rem File "d:\checkouts\D-FAST\D-FAST_Morphological_Impact\dfastmi.dist\pyproj\datadir.py", line 109, in get_data_dir
+rem  pyproj.exceptions.DataDirError: Valid PROJ data directory not found. Either set the path using the environmental variable PROJ_LIB or with `pyproj.datadir.set_data_dir`.
+mkdir dfastmi.dist\proj
+copy ..\envs\dfastbe\Library\share\proj\* dfastmi.dist\proj
+
+rem File "d:\checkouts\D-FAST\D-FAST_Morphological_Impact\dfastmi.dist\rtree\core.py", line 126, in <module rtree.core>
+rem  OSError: could not find or load spatialindex_c-64.dll
+mkdir dfastmi.dist\Library\bin
+copy ..\envs\dfastbe\Library\bin\spatialindex* dfastmi.dist\Library\bin
+
+rem File "d:\checkouts\D-FAST\D-FAST_Morphological_Impact\dfastmi.dist\shapely\geos.py", line 60, in load_dll
+rem  OSError: Could not find lib geos_c.dll or load any of its variants [].
+mkdir dfastmi.dist\shapely\DLLs
+copy ..\envs\dfastbe\Library\bin\geos* dfastmi.dist\shapely\DLLs
+
+rem   File "d:\checkouts\D-FAST\D-FAST_Morphological_Impact\dfastmi.dist\geopandas\datasets\__init__.py", line 6, in <module geopandas.datasets>
+rem StopIteration
+mkdir dfastmi.dist\geopandas\datasets
+copy ..\envs\dfastbe\Lib\site-packages\geopandas\datasets\natural* dfastmi.dist\geopandas\datasets
+
+rem File "d:\checkouts\D-FAST\D-FAST_Morphological_Impact\dfastmi.dist\PyQt5\__init__.py", line 33, in find_qt
+rem  ImportError: unable to find Qt5Core.dll on PATH
+mkdir dfastmi.dist\PyQt5\Qt\bin
+copy ..\envs\dfastbe\Library\bin\Qt5Core* dfastmi.dist\PyQt5\Qt\bin
+
 rem '...\dfastmi_usermanual.pdf' is not recognized as an internal or external command, operable program or batch file.
 copy docs\dfastmi_usermanual.pdf dfastmi.dist\dfastmi
