8cf50a8abeae90a495c28b0ae6b8bfb6ad7352c3
Marlon Vermeulen <Marlon.Vermeulen@deltares.nl>
2023-02-02 11:21:43 +0100

    Add extra description in build scripts with which error it resolves

--- BuildScripts/CopyLanguageFile.bat
+++ BuildScripts/CopyLanguageFile.bat
@@ -1,4 +1,5 @@
 rem Load language file 'messages.UK.ini'
+rem Resolves the following error: Unable to load language file 'messages.UK.ini'
 cd ..
 mkdir dfastmi.dist\dfastmi
 copy dfastmi\messages.*.ini dfastmi.dist\dfastmi
