e18c4eb6105fd6abb6c555fca4833fb0bd6af946
ralph-peelen-deltares <ralph.peelen@deltares.nl>
2024-01-22 14:17:46 +0100

    DFAST-77: Setup install and configuration scripts

---
+++ BuildScripts/CondaInstall.bat
@@ -0,0 +1,14 @@
+@echo on
+
+rem Set the Miniconda installer file name
+set INSTALLER_FILENAME=Miniconda3-latest-Windows-x86_64.exe
+
+rem Download the Miniconda installer
+curl -O https://repo.anaconda.com/miniconda/%INSTALLER_FILENAME%
+
+rem Run the installer with silent/unattended installation
+CALL %INSTALLER_FILENAME% /S /InstallationType=JustMe /AddToPath=1 /RegisterPython=0 /NoRegistry=1 /NoDesktop=1 /D=%UserProfile%\Miniconda3
+
+rem Clean up the installer file
+del %INSTALLER_FILENAME%
+
---
+++ BuildScripts/DevelopDfastmi.bat
@@ -0,0 +1,50 @@
+@echo on
+SET CONDA_ENV_NAME=py_3_9_12-dfastmi
+setlocal
+:START
+where conda > nul 2>nul
+if %errorlevel% neq 0 (
+    echo Conda is not installed on this PC.
+    echo Please install Conda and try again.
+	:PROMPT
+	SET /P AREYOUSURE=Are you sure you want to install conda now (Y/[N])?
+	IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+	CALL CondaInstall.bat
+	GOTO START
+) else (
+    echo Conda is installed on this PC.
+)
+	CALL conda deactivate
+	CALL conda remove -v -y --name %CONDA_ENV_NAME% --all
+	CALL conda create -v -y -n %CONDA_ENV_NAME% python=3.9.12
+	CALL conda activate %CONDA_ENV_NAME%
+	CALL python -m pip install --upgrade pip
+	CALL python -m pip install poetry
+	CALL python -m poetry install --no-root
+)
+where code > nul 2>nul
+if %errorlevel% neq 0 (
+    echo visual studio code is not installed on this PC.
+    echo Please install visual studio code and try again.
+) else (
+    echo visual studio code is installed on this PC.
+
+	CALL code --install-extension Cameron.vscode-pytest --force
+	CALL code --install-extension donjayamanne.python-environment-manager --force
+	CALL code --install-extension hbenl.vscode-test-explorer --force
+	CALL code --install-extension littlefoxteam.vscode-python-test-adapter --force
+	CALL code --install-extension ms-python.pylint --force
+	CALL code --install-extension ms-python.python --force
+	CALL code --install-extension ms-python.vscode-pylance --force
+	#CALL code --install-extension ms-toolsai.jupyter --force
+	#CALL code --install-extension ms-toolsai.jupyter-keymap --force
+	#CALL code --install-extension ms-toolsai.jupyter-renderers --force
+	#CALL code --install-extension ms-toolsai.vscode-jupyter-cell-tags --force
+	#CALL code --install-extension ms-toolsai.vscode-jupyter-slideshow --force
+	#CALL code --install-extension ms-vscode.live-server --force
+	CALL code --install-extension ms-vscode.test-adapter-converter --force
+	CALL code --install-extension ryanluker.vscode-coverage-gutters --force
+)
+
+:END
+endlocal
