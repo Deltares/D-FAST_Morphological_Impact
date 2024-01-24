da6a7a74c5e09cee868997111491dbf30817e44d
ralph-peelen-deltares <ralph.peelen@deltares.nl>
2024-01-24 16:12:47 +0100

    DFAST-80 update buildscript

--- BuildScripts/BuildDfastmi.bat
+++ BuildScripts/BuildDfastmi.bat
@@ -7,21 +7,39 @@ START /B /WAIT poetry run nuitka ^
  --standalone ^
  --assume-yes-for-downloads ^
  --python-flag=no_site ^
+ --python-flag=no_asserts ^
+ --python-flag=no_docstrings ^
  --show-progress ^
+ --windows-force-stderr-spec=%PROGRAM%logs.txt ^
+ --windows-force-stdout-spec=%PROGRAM%output.txt ^
+ --nofollow-import-to=*.tests ^
+ --follow-stdlib ^
+ --enable-plugin=anti-bloat ^
+ --enable-plugin=delvewheel ^
  --enable-plugin=pyqt5 ^
+ --enable-plugin=matplotlib ^
  --file-reference-choice=runtime ^
  --include-package=pyproj ^
- --include-module=pyproj ^
- --include-module=shapely ^
- --include-package=matplotlib ^
+ --include-package=shapely ^
  --include-package=netCDF4 ^
- --include-module=geopandas ^
+ --include-package=geopandas ^
  --include-package-data=geopandas.datasets ^
- --include-module=fiona ^
  --include-package=fiona ^
+ --include-data-files=dfastmi/Dutch_rivers.ini=Dutch_rivers.ini ^
+ --include-data-files=dfastmi/messages.NL.ini=messages.NL.ini^
+ --include-data-files=dfastmi/messages.UK.ini=messages.UK.ini^
+ --include-data-files=dfastmi/open.png=open.png^
+ --include-data-files=docs/dfastmi_usermanual.pdf=dfastmi_usermanual.pdf^
+ --output-dir=dfastmi.install ^
+ --company-name=Deltares ^
+ --file-version=3.0.0 ^
+ --product-version=2024.01.3.0 ^
+ --product-name="D-FAST Morphological Impact" ^
+ --file-description="A Python to perform a morphological impact analysis based on a number of D-Flow FM simulations." ^
+ --trademarks="All indications and logos of, and references to, \"D-FAST\", \"D-FAST Morphological Impact\" and \"D-FAST MI\" are registered trademarks of Stichting Deltares, and remain the property of Stichting Deltares. All rights reserved." ^
+ --copyright="Copyright (C) 2020 Stichting Deltares." ^
+ --onefile ^
+ --force-dll-dependency-cache-update ^
  dfastmi
 
-cd %~dp0
-call PostBuild.bat
-
-rem end of build
+cd %~dp0/../dfastmi.install
