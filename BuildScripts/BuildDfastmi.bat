51de999d414fce7bfdb4d77a7a1e727eb32a7d07
ralph-peelen-deltares <ralph.peelen@deltares.nl>
2024-01-26 12:11:36 +0100

    DFAST-81: Update nuitka build config to include details/company information

--- BuildScripts/BuildDfastmi.bat
+++ BuildScripts/BuildDfastmi.bat
@@ -17,6 +17,13 @@ START /B /WAIT poetry run nuitka ^
  --include-module=geopandas ^
  --include-package-data=geopandas.datasets ^
  --include-module=fiona ^
+ --company-name=Deltares ^
+ --file-version=3.0.0 ^
+ --product-version=2024.01.3.0 ^
+ --product-name="D-FAST Morphological Impact" ^
+ --file-description="A Python to perform a morphological impact analysis based on a number of D-Flow FM simulations." ^
+ --trademarks="All indications and logos of, and references to, \"D-FAST\", \"D-FAST Morphological Impact\" and \"D-FAST MI\" are registered trademarks of Stichting Deltares, and remain the property of Stichting Deltares. All rights reserved." ^
+ --copyright="Copyright (C) 2020 Stichting Deltares." ^
  dfastmi
 
 cd %~dp0
