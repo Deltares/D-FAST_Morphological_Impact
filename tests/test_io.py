a46025dcf855db283c71c11c19a792611d04cab5
ralph-peelen-deltares <ralph.peelen@deltares.nl>
2024-02-05 16:52:09 +0100

    DFAST-86: write_config naming improved + convert to mocked unit tests

--- tests/test_io.py
+++ tests/test_io.py
@@ -126,7 +126,7 @@ class Test_log_text():
         with mock.patch("builtins.open") as mock_file:
             dfastmi.io.log_text(key, dict=dict, file=mock_file)
             assert mock_file.write.called
-            mock_file.write.assert_called_once_with("Confirm key found with ABC\n")            
+            mock_file.write.assert_called_once_with("Confirm key found with ABC\n")    
 
 class Test_data_access_log_text():
     @pytest.fixture
@@ -247,7 +247,38 @@ class Test_data_access_get_text():
         assert confirmText == ['Confirm using "y" ...','']
 
 class Test_write_config():
-    def test_write_config_01(self):
+    def test_write_config_check_written(self):
+        """
+        Testing write_config.
+        """
+        filename = "test.cfg"
+        config = configparser.ConfigParser()
+        config.add_section("G 1")
+        config["G 1"]["K 1"] = "V 1"
+        config.add_section("Group 2")
+        config["Group 2"]["K1"] = "1.0 0.1 0.0 0.01"
+        config["Group 2"]["K2"] = "2.0 0.2 0.02 0.0"
+        config.add_section("Group 3")
+        config["Group 3"]["LongKey"] = "3"
+
+        with mock.patch("builtins.open") as mock_file:
+            dfastmi.io.write_config(filename, config)
+            mock_file.assert_called_once_with(filename, 'w')
+            mock_file.return_value.__enter__().write.assert_called()
+            mock_file.return_value.__enter__().write.assert_any_call('[G 1]\n')
+            mock_file.return_value.__enter__().write.assert_any_call('  k 1     = V 1\n')
+            mock_file.return_value.__enter__().write.assert_any_call('\n')
+            mock_file.return_value.__enter__().write.assert_any_call('[Group 2]\n')
+            mock_file.return_value.__enter__().write.assert_any_call('  k1      = 1.0 0.1 0.0 0.01\n')
+            mock_file.return_value.__enter__().write.assert_any_call('  k2      = 2.0 0.2 0.02 0.0\n')
+            mock_file.return_value.__enter__().write.assert_any_call('[Group 3]\n')
+            mock_file.return_value.__enter__().write.assert_any_call('  longkey = 3\n')
+            
+
+        
+
+class Test_data_access_write_config():
+    def test_write_config_and_read_back(self):
         """
         Testing write_config.
         """
@@ -272,7 +303,6 @@ class Test_write_config():
                          '[Group 3]',
                          '  longkey = 3']
         assert all_lines == all_lines_ref
-
 class Test_read_fm_map():
     def test_read_fm_map_01(self):
         """
