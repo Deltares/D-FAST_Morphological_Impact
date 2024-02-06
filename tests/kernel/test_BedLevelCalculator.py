63f952e4afd96626a5f7312a242b04f4fcc8dda9
Marlon Vermeulen <Marlon.Vermeulen@deltares.nl>
2024-02-06 12:57:24 +0100

    DFAST-87: add BedLevelCalculator tests

---
+++ tests/kernel/test_BedLevelCalculator.py
@@ -0,0 +1,62 @@
+from dfastmi.kernel.BedLevelCalculator import BedLevelCalculator
+import numpy
+import pytest
+
+class Test_BedLevelCalculator():
+    def test_given_array_with_value_when_get_element_wise_maximum_then_return_maximum_array(self):
+        dummy_array = [numpy.array([0, 0, 0]), numpy.array([0, 0, 0]), numpy.array([0, 0, 0])]
+        blc = BedLevelCalculator(dummy_array)
+        maximum_array = numpy.array([40, 45, 50, 1., 0.])
+        
+        zdzb = [numpy.array([5, 10, 20, 1., 0.]),
+                numpy.array([25, 30, 35, 1., 0.]),
+                maximum_array]
+        
+        
+        element_wise_maximum = blc.get_element_wise_maximum(zdzb)
+        assert numpy.array_equal(element_wise_maximum, maximum_array)
+        
+    def test_given_array_with_value_when_get_element_wise_minimum_then_return_minimum_array(self):
+        dummy_array = [numpy.array([0, 0, 0]), numpy.array([0, 0, 0]), numpy.array([0, 0, 0])]
+        blc = BedLevelCalculator(dummy_array)
+        minimum_array = numpy.array([5, 10, 20, 1., 0.])
+        
+        dzb =  [minimum_array,
+                numpy.array([25, 30, 35, 1., 0.]),
+                numpy.array([40, 45, 50, 1., 0.])]
+        
+        
+        element_wise_minimum = blc.get_element_wise_minimum(dzb)
+        assert numpy.array_equal(element_wise_minimum, minimum_array)
+        
+    @pytest.mark.parametrize("number_of_days, linear_average_array", [
+        ((0.5, 0.25, 0.25, 0.25), numpy.array([21.25, 26.25, 33.125, 1., 0.])),
+        ((0.25, 0.25, 0.25, 0.25), numpy.array([17.5, 21.25, 26.25, 0.75, 0.])),
+    ])    
+    def test_given_array_with_value_and_varying_number_of_days_when_linear_average_then_return_linear_average_array(self, number_of_days, linear_average_array):
+        dummy_array = [numpy.array([0, 0, 0]), numpy.array([0, 0, 0]), numpy.array([0, 0, 0])]
+        blc = BedLevelCalculator(dummy_array)
+
+        dzb =  [numpy.array([5, 10, 20, 1., 0.]),
+                numpy.array([25, 30, 35, 1., 0.]),
+                numpy.array([40, 45, 50, 1., 0.])]
+        
+        linear_average = blc.linear_average(number_of_days, dzb)
+        assert numpy.array_equal(linear_average, linear_average_array)
+      
+      
+    @pytest.mark.parametrize("rsigma, expected_bed_level_change", [
+        ((2, 1, 1), [numpy.array([ 5., 10., 20.,  1., -0.]), numpy.array([ 5., 10., 20.,  1., -0.]), numpy.array([ 5., 10., 20.,  1., -0.])]),
+        ((0, 0, 0), [numpy.array([40, 45, 50, 1., 0.]), numpy.array([5, 10, 20, 1., 0.]), numpy.array([25, 30, 35, 1., 0.])]),
+        ((.2, 0, 1), [numpy.array([25., 30., 35.,  1.,  0.]), numpy.array([ 9., 14., 23.,  1.,  0.]), numpy.array([25., 30., 35.,  1.,  0.])]),
+        ((1, .2, .2), [numpy.array([37.5, 42.5, 47.5,  1. ,  0. ]), numpy.array([37.5, 42.5, 47.5,  1. ,  0. ]), numpy.array([27.5, 32.5, 37.5,  1. ,  0. ])]),
+    ])   
+    def test_given_array_with_value_and_varying_rsigma_when_get_bed_level_changes_then_return_bed_level_change(self, rsigma, expected_bed_level_change):
+        dzq =  [numpy.array([5, 10, 20, 1., 0.]),
+                numpy.array([25, 30, 35, 1., 0.]),
+                numpy.array([40, 45, 50, 1., 0.])]
+        
+        blc = BedLevelCalculator(dzq)
+        
+        bed_level_change = blc.get_bed_level_changes(dzq, rsigma)
+        assert numpy.allclose(bed_level_change, expected_bed_level_change, atol=1e-6)
