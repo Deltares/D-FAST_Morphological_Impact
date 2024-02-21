ae96860ec25d5ad7809beb879e60738ce703550b
Marlon Vermeulen <Marlon.Vermeulen@deltares.nl>
2024-02-21 09:38:26 +0100

    DFAST-104 move AnalyserAndReporterWaqua.py

--- dfastmi/batch.py
+++ dfastmi/batch.py
@@ -39,6 +39,7 @@ from dfastmi.io.GridOperations import GridOperations
 from dfastmi.io.FileUtils import FileUtils
 from dfastmi.io.ConfigFileOperations import ConfigFileOperations
 
+from dfastmi.batch_dir import AnalyserAndReporterWaqua
 from dfastmi.batch_dir import ConfigurationChecker
 from dfastmi.batch_dir import DetectAndPlot
 from dfastmi.batch_dir import FileNameRetriever
@@ -620,7 +621,7 @@ def analyse_and_report(
         Flag indicating whether analysis could be carried out.
     """
     if imode == 0:
-        return analyse_and_report_waqua(
+        return AnalyserAndReporterWaqua.analyse_and_report_waqua(
             display,
             report,
             reduced_output,
@@ -662,114 +663,6 @@ def analyse_and_report(
             plotops,
         )
 
-
-def analyse_and_report_waqua(
-    display: bool,
-    report: TextIO,
-    reduced_output: bool,
-    reach: str,
-    q_location: str,
-    tstag: float,
-    Q: Vector,
-    apply_q: BoolVector,
-    T: Vector,
-    rsigma: Vector,
-    slength: float,
-    ucrit: float,
-    old_zmin_zmax: bool,
-    outputdir: str
-) -> bool:
-    """
-    Perform analysis based on WAQUA data.
-
-    Read data from samples files exported from WAQUA simulations, perform
-    analysis and write the results to three SIMONA boxfiles.
-
-    Arguments
-    ---------
-    display : bool
-        Flag indicating text output to stdout.
-    report : TextIO
-        Text stream for log file.
-    reduced_output : bool
-        Flag to indicate whether WAQUA output should be reduced to the area of
-        interest only.
-    reach : str
-        Name of the reach.
-    q_location : str
-        Name of the location at which the discharge is
-    tstag : float
-        Number of days that the river is stagnant.
-    Q : Vector
-        Array of discharges; one for each forcing condition.
-    apply_q : BoolVector
-        A tuple of 3 flags indicating whether each value should be used or not.
-    T : Vector
-        Fraction of year represented by each forcing condition.
-    rsigma : Vector
-        Array of relaxation factors; one per forcing condition.
-    slength : float
-        The expected yearly impacted sedimentation length.
-    ucrit : float
-        Critical flow velocity [m/s].
-    old_zmin_zmax : bool
-        Specifies the minimum and maximum should follow old or new definition.
-    outputdir : str
-        Name of the output directory.
-
-    Returns
-    -------
-    success : bool
-        Flag indicating whether analysis could be carried out.
-    """
-    success = True
-    first_discharge = True
-    
-    dzq : List[Optional[Union[float, numpy.ndarray]]]
-    dzq = [None] * len(Q)
-    for i in range(3):
-        if success and apply_q[i]:
-            if first_discharge:
-                dzq[i], firstm, firstn = get_values_waqua3(
-                    i+1, Q[i], ucrit, display, report, reduced_output
-                )
-                first_discharge = False
-            else:
-                dzq[i] = get_values_waqua1(i+1, Q[i], ucrit, display, report, reduced_output)
-            if dzq[i] is None:
-                success = False
-        else:
-            dzq[i] = 0.0
-
-    if success:
-        if display:
-            ApplicationSettingsHelper.log_text("char_bed_changes")
-            
-        if tstag > 0:
-            dzq = [dzq[0], 0.0, dzq[1], dzq[2]]
-            T = (T[0], tstag, T[1], T[2])
-            rsigma = (rsigma[0], 1.0, rsigma[1], rsigma[2])
-        
-        # main_computation now returns new pointwise zmin and zmax
-        data_zgem, data_zmax, data_zmin, dzb = dfastmi.kernel.core.main_computation(
-            dzq, T, rsigma
-        )
-        if old_zmin_zmax:
-            # get old zmax and zmin
-            data_zmax = dzb[0]
-            data_zmin = dzb[1]
-
-        DataTextFileOperations.write_simona_box(
-            outputdir + os.sep + ApplicationSettingsHelper.get_filename("avgdzb.out"), data_zgem, firstm, firstn
-        )
-        DataTextFileOperations.write_simona_box(
-            outputdir + os.sep + ApplicationSettingsHelper.get_filename("maxdzb.out"), data_zmax, firstm, firstn
-        )
-        DataTextFileOperations.write_simona_box(
-            outputdir + os.sep + ApplicationSettingsHelper.get_filename("mindzb.out"), data_zmin, firstm, firstn
-        )
-
-    return success
 
 
 def analyse_and_report_dflowfm(
@@ -1247,136 +1140,6 @@ def analyse_and_report_dflowfm(
             )
         
     return not missing_data
-
-
-def get_values_waqua1(
-    stage: int, q: float, ucrit: float, display: bool, report, reduced_output: bool
-) -> numpy.ndarray:
-    """
-    Read data files exported from WAQUA for the specified stage, and return equilibrium bed level change.
-
-    Arguments
-    ---------
-    stage : int
-        Discharge level (1, 2 or 3).
-    q : float
-        Discharge value.
-    ucrit : float
-        Critical flow velocity.
-    display : bool
-        Flag indicating text output to stdout.
-    report : TextIO
-        Text stream for log file.
-    reduced_output : bool
-        Flag to indicate whether WAQUA output should be reduced to the area of
-        interest only.
-
-    Returns
-    -------
-    dzq : numpy.ndarray
-        Array containing equilibrium bed level change.
-    """
-    dzq, firstm, firstn = get_values_waqua3(
-        stage, q, ucrit, display, report, reduced_output
-    )
-    return dzq
-
-
-def get_values_waqua3(
-    stage: int, q: float, ucrit: float, display: bool, report, reduced_output: bool
-) -> Tuple[numpy.ndarray, int, int]:
-    """
-    Read data files exported from WAQUA for the specified stage, and return equilibrium bed level change and minimum M and N.
-
-    Arguments
-    ---------
-    stage : int
-        Discharge level (1, 2 or 3).
-    q : float
-        Discharge value.
-    ucrit : float
-        Critical flow velocity.
-    display : bool
-        Flag indicating text output to stdout.
-    report : TextIO
-        Text stream for log file.
-    reduced_output : bool
-        Flag to indicate whether WAQUA output should be reduced to the area of
-        interest only.
-
-    Returns
-    -------
-    dzq : numpy.ndarray
-        Array containing equilibrium bed level change.
-    firstm : int
-        Minimum M index read (0 if reduced_output is False).
-    firstn : int
-        Minimum N index read (0 if reduced_output is False).
-    """
-    cblok = str(stage)
-    if display:
-        ApplicationSettingsHelper.log_text("input_xyz", dict={"stage": stage, "q": q})
-        ApplicationSettingsHelper.log_text("---")
-        ApplicationSettingsHelper.log_text("")
-
-    discriptions = ApplicationSettingsHelper.get_text("file_descriptions")
-    quantities = ["velocity-zeta.001", "waterdepth-zeta.001", "velocity-zeta.002"]
-    files = []
-    for i in range(3):
-        if display:
-            ApplicationSettingsHelper.log_text("input_xyz_name", dict={"name": discriptions[i]})
-        cifil = "xyz_" + quantities[i] + ".Q" + cblok + ".xyz"
-        if display:
-            print(cifil)
-        if not os.path.isfile(cifil):
-            ApplicationSettingsHelper.log_text("file_not_found", dict={"name": cifil})
-            ApplicationSettingsHelper.log_text("file_not_found", dict={"name": cifil}, file=report)
-            ApplicationSettingsHelper.log_text("end_program", file=report)
-            return numpy.array([]), 0, 0
-        else:
-            if display:
-                ApplicationSettingsHelper.log_text("input_xyz_found", dict={"name": cifil})
-        files.extend([cifil])
-        if display:
-            ApplicationSettingsHelper.log_text("")
-
-    if display:
-        ApplicationSettingsHelper.log_text("input_xyz_read", dict={"stage": stage})
-    u0temp = DataTextFileOperations.read_waqua_xyz(files[0], cols=(2, 3, 4))
-    m = u0temp[:, 1].astype(int) - 1
-    n = u0temp[:, 2].astype(int) - 1
-    u0temp = u0temp[:, 0]
-    h0temp = DataTextFileOperations.read_waqua_xyz(files[1])
-    u1temp = DataTextFileOperations.read_waqua_xyz(files[2])
-
-    if reduced_output:
-        firstm = min(m)
-        firstn = min(n)
-    else:
-        firstm = 0
-        firstn = 0
-    szm = max(m) + 1 - firstm
-    szn = max(n) + 1 - firstn
-    szk = szm * szn
-    k = szn * m + n
-    u0 = numpy.zeros([szk])
-    h0 = numpy.zeros([szk])
-    u1 = numpy.zeros([szk])
-
-    u0[k] = u0temp
-    h0[k] = h0temp
-    u1[k] = u1temp
-
-    sz = [szm, szn]
-    u0 = u0.reshape(sz)
-    h0 = h0.reshape(sz)
-    u1 = u1.reshape(sz)
-
-    dzq = dfastmi.kernel.core.dzq_from_du_and_h(u0, h0, u1, ucrit)
-    if display:
-        ApplicationSettingsHelper.log_text("---")
-    return dzq, firstm, firstn
-
 
 def get_values_fm(
     stage: int,
