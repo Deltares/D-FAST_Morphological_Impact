54d18550ecd8e7f783c484243d78268cd4bfaefc
Marlon Vermeulen <Marlon.Vermeulen@deltares.nl>
2024-03-01 09:33:37 +0100

    DFAST-121 Move test classes to seperate files

--- tests/batch/test_FileNameRetriever.py
+++ tests/batch/test_FileNameRetriever.py
@@ -1,281 +1,128 @@
-import pytest
 from configparser import ConfigParser
-from packaging import version
+import pytest
 from dfastmi.batch.FileNameRetriever import FileNameRetriever
-from dfastmi.batch.FileNameRetrieverFactory import FileNameRetrieverFactory
-from dfastmi.batch.FileNameRetrieverLegacy import FileNameRetrieverLegacy
-from dfastmi.batch.FileNameRetrieverUnsupported import FileNameRetrieverUnsupported
 
 class Test_FileNameRetriever():
-    class Test_FileNameRetrieverFactory():
-        @pytest.fixture
-        def factory(self) -> FileNameRetrieverFactory:
-            factory = FileNameRetrieverFactory()
-            factory.register_creator(version.Version("1.0"), lambda needs_tide: FileNameRetrieverLegacy())
-            factory.register_creator(version.Version("2.0"), lambda needs_tide: FileNameRetriever(needs_tide))
-            return factory
+    @pytest.mark.parametrize("use_tide", [
+        True,
+        False
+    ])
+    def given_partial_setup_config_parser_when_get_file_names_then_throw_key_error_with_expected_message(self, use_tide):
+        """
+        given : partial setup config parser
+        when :  get file names
+        then  : throw key error with expected message
+        """
+        file_name_retriever = FileNameRetriever(use_tide)
         
-        @pytest.mark.parametrize("string_version", [
-            "1",
-            "1.0",
-            "1.0.0",
-            "1.0.0.0"
-        ])
-        def given_varying_version_1_when_generate_then_return_FileNameRetrieverLegacy(self, factory : FileNameRetrieverFactory, string_version : str):
-            """
-            given : varying version 1
-            when :  generate 
-            then  : return FileNameRetrieverLegacy
-            """
-            file_name_retriever_version = version.Version(string_version)
-            needs_tide = True
-            file_name_retriever = factory.generate(file_name_retriever_version, needs_tide)
-            assert isinstance(file_name_retriever, FileNameRetrieverLegacy)
+        key = "Discharge"
+        chap = "C1"
+        expected_exception_message = f'Keyword "{key}" is not specified in group "{chap}" of analysis configuration file.'
         
-        @pytest.mark.parametrize("needs_tide", [
-            True,
-            False
-        ])
-        def given_version_2_with_varying_needs_tide_when_generate_then_return_FileNameRetriever(self, factory : FileNameRetrieverFactory, needs_tide : bool):
-            """
-            given : version 2 with varying needs tide
-            when :  generate 
-            then  : return FileNameRetriever
-            """
-            file_name_retriever_version = version.Version("2.0")
-            file_name_retriever = factory.generate(file_name_retriever_version, needs_tide)
-            assert isinstance(file_name_retriever, FileNameRetriever)
+        config = ConfigParser()
+        config.add_section(chap)
         
-        @pytest.mark.parametrize("string_version", [
-            "2",
-            "2.0",
-            "2.0.0",
-            "2.0.0.0"
-        ])    
-        def given_varying_version_2_when_generate_then_return_FileNameRetrieverLegacy(self, factory : FileNameRetrieverFactory, string_version : str):
-            """
-            given : varying version 2
-            when :  generate 
-            then  : return FileNameRetrieverLegacy
-            """
-            file_name_retriever_version = version.Version(string_version)
-            file_name_retriever = factory.generate(file_name_retriever_version, True)
-            assert isinstance(file_name_retriever, FileNameRetriever)
+        with pytest.raises(KeyError) as e:
+            file_name_retriever.get_file_names(config)
+        assert str(e.value.args[0]) == expected_exception_message
         
-        @pytest.mark.parametrize("string_version", [
-            "0.0",
-            "999.0"
-        ])
-        def given_unsupported_version_when_generate_then_return_FileNameRetrieverUnsupported(self, factory : FileNameRetrieverFactory, string_version : str):
-            """
-            given : unsupported version
-            when :  generate 
-            then  : return FileNameRetrieverUnsupported
-            """
-            needs_tide = True
-            file_name_retriever_version = version.Version(string_version)
-            file_name_retriever = factory.generate(file_name_retriever_version, needs_tide)
-            assert isinstance(file_name_retriever, FileNameRetrieverUnsupported)
+    @pytest.mark.parametrize("not_a_float_string", [
+        "not a float",
+        "1,0",
+        "#"
+        "--=+"
+    ])
+    def given_config_parser_with_not_a_float_for_discharge_when_get_file_names_then_throw_type_error_with_expected_message(self, not_a_float_string):
+        """
+        given : config parser with not a float for discharge
+        when :  get file names
+        then  : throw type error with expected message
+        """
+        file_name_retriever = FileNameRetriever(False)
+        
+        key = "Discharge"
+        chap = "C1"
+        expected_exception_message = f'{not_a_float_string} from Discharge could now be handled as a float.'
+        
+        config = ConfigParser()
+        config.add_section(chap)
+        config.set(chap, key, not_a_float_string)
+        
+        with pytest.raises(TypeError) as e:
+            file_name_retriever.get_file_names(config)
+        assert str(e.value) == expected_exception_message
     
-    class Test_FileNameRetriever_Unsupported():
-        def given_config_parser_when_get_file_names_unsupported_then_return_no_file_names(self):
-            """
-            given : config parser
-            when :  get file names unsupported 
-            then  : return no file names
-            """
-            config = ConfigParser()
-            fnrvu = FileNameRetrieverUnsupported()
-            
-            filenames = fnrvu.get_file_names(config)
-            
-            assert len(filenames) == 0
+    @pytest.mark.parametrize("use_tide", [
+        True,
+        False
+    ])
+    def given_empty_config_parser_when_get_file_names_then_return_no_file_names(self, use_tide):
+        """
+        given : empty config parser
+        when :  get file names
+        then  : return no file names
+        """
+        config = ConfigParser()
+        file_name_retriever = FileNameRetriever(use_tide)
+        
+        filenames = file_name_retriever.get_file_names(config)
+        
+        assert len(filenames) == 0
     
-    class Test_FileNameRetriever_legacy():
-        def given_partial_setup_config_parser_when_get_file_names_legacy_then_throw_key_error_with_expected_message(self):
-            """
-            given : partial setup config parser
-            when :  get file names legacy
-            then  : throw key error with expected message
-            """
-            fnr_legacy = FileNameRetrieverLegacy()
-            
-            key = "Reference"
-            chap = "Q1"
-            expected_exception_message = f'Keyword "{key}" is not specified in group "{chap}" of analysis configuration file.'
-            
-            config = ConfigParser()
-            config.add_section(chap)
-            
-            with pytest.raises(KeyError) as e:
-                fnr_legacy.get_file_names(config)
-            assert str(e.value.args[0]) == expected_exception_message
+    def given_config_parser_with_data_when_get_file_names_then_return_expected_file_names(self):
+        """
+        given : config parser with data
+        when :  get file names
+        then  : return expected file names
+        """
+        config = ConfigParser()
+        file_name_retriever = FileNameRetriever(False)
         
-        def given_empty_config_parser_when_get_file_names_legacy_then_return_no_file_names(self):
-            """
-            given : empty config parser
-            when :  get file names legacy
-            then  : return no file names
-            """
-            config = ConfigParser()
-            fnr_legacy = FileNameRetrieverLegacy()
-            
-            filenames = fnr_legacy.get_file_names(config)
-            
-            assert len(filenames) == 0
+        q1_expected_filename= self.get_expected_q_filename_and_update_config(config, "C1", "1.0")
+        q2_expected_filename= self.get_expected_q_filename_and_update_config(config, "C2", "2.0")
+        q3_expected_filename= self.get_expected_q_filename_and_update_config(config, "C3", "3.0")
         
-        def given_config_parser_with_data_when_get_file_names_legacy_then_return_expected_file_names(self):
-            """
-            given : config parser with data
-            when :  get file names legacy
-            then  : return expected file names
-            """
-            config = ConfigParser()
-            fnr_legacy = FileNameRetrieverLegacy()
-            
-            q1_expected_filename= self.get_expected_q_filename_and_update_config(config, "Q1")
-            q2_expected_filename= self.get_expected_q_filename_and_update_config(config, "Q2")
-            q3_expected_filename= self.get_expected_q_filename_and_update_config(config, "Q3")
-            
-            filenames = fnr_legacy.get_file_names(config)
-            
-            assert filenames[0] == q1_expected_filename
-            assert filenames[1] == q2_expected_filename
-            assert filenames[2] == q3_expected_filename
-
-        def get_expected_q_filename_and_update_config(self, config : ConfigParser, q : str):
-            reference = "Reference"
-            with_measure = "WithMeasure"
-            q_reference_filename = q + "-" + reference
-            q_with_measure_filename = q + "-" + with_measure
-            
-            config.add_section(q)
-            config.set(q, reference, q_reference_filename)
-            config.set(q, with_measure, q_with_measure_filename)
-            
-            return (q_reference_filename,q_with_measure_filename)
+        filenames = file_name_retriever.get_file_names(config)
         
-    class Test_FileNameRetriever():
-        @pytest.mark.parametrize("use_tide", [
-            True,
-            False
-        ])
-        def given_partial_setup_config_parser_when_get_file_names_then_throw_key_error_with_expected_message(self, use_tide):
-            """
-            given : partial setup config parser
-            when :  get file names
-            then  : throw key error with expected message
-            """
-            file_name_retriever = FileNameRetriever(use_tide)
-            
-            key = "Discharge"
-            chap = "C1"
-            expected_exception_message = f'Keyword "{key}" is not specified in group "{chap}" of analysis configuration file.'
-            
-            config = ConfigParser()
-            config.add_section(chap)
-            
-            with pytest.raises(KeyError) as e:
-                file_name_retriever.get_file_names(config)
-            assert str(e.value.args[0]) == expected_exception_message
-            
-        @pytest.mark.parametrize("not_a_float_string", [
-            "not a float",
-            "1,0",
-            "#"
-            "--=+"
-        ])
-        def given_config_parser_with_not_a_float_for_discharge_when_get_file_names_then_throw_type_error_with_expected_message(self, not_a_float_string):
-            """
-            given : config parser with not a float for discharge
-            when :  get file names
-            then  : throw type error with expected message
-            """
-            file_name_retriever = FileNameRetriever(False)
-            
-            key = "Discharge"
-            chap = "C1"
-            expected_exception_message = f'{not_a_float_string} from Discharge could now be handled as a float.'
-            
-            config = ConfigParser()
-            config.add_section(chap)
-            config.set(chap, key, not_a_float_string)
-            
-            with pytest.raises(TypeError) as e:
-                file_name_retriever.get_file_names(config)
-            assert str(e.value) == expected_exception_message
+        assert filenames[1.0] == q1_expected_filename
+        assert filenames[2.0] == q2_expected_filename
+        assert filenames[3.0] == q3_expected_filename
         
-        @pytest.mark.parametrize("use_tide", [
-            True,
-            False
-        ])
-        def given_empty_config_parser_when_get_file_names_then_return_no_file_names(self, use_tide):
-            """
-            given : empty config parser
-            when :  get file names
-            then  : return no file names
-            """
-            config = ConfigParser()
-            file_name_retriever = FileNameRetriever(use_tide)
-            
-            filenames = file_name_retriever.get_file_names(config)
-            
-            assert len(filenames) == 0
+    def given_config_parser_with_data_and_need_of_tide_enable_when_get_file_names_then_return_expected_file_names_including_tide(self):
+        """
+        given : config parser with data and need of tide enable
+        when :  get file names
+        then  : return expected file names including tide
+        """
+        config = ConfigParser()
+        file_name_retriever = FileNameRetriever(True)
         
-        def given_config_parser_with_data_when_get_file_names_then_return_expected_file_names(self):
-            """
-            given : config parser with data
-            when :  get file names
-            then  : return expected file names
-            """
-            config = ConfigParser()
-            file_name_retriever = FileNameRetriever(False)
-            
-            q1_expected_filename= self.get_expected_q_filename_and_update_config(config, "C1", "1.0")
-            q2_expected_filename= self.get_expected_q_filename_and_update_config(config, "C2", "2.0")
-            q3_expected_filename= self.get_expected_q_filename_and_update_config(config, "C3", "3.0")
-            
-            filenames = file_name_retriever.get_file_names(config)
-            
-            assert filenames[1.0] == q1_expected_filename
-            assert filenames[2.0] == q2_expected_filename
-            assert filenames[3.0] == q3_expected_filename
-            
-        def given_config_parser_with_data_and_need_of_tide_enable_when_get_file_names_then_return_expected_file_names_including_tide(self):
-            """
-            given : config parser with data and need of tide enable
-            when :  get file names
-            then  : return expected file names including tide
-            """
-            config = ConfigParser()
-            file_name_retriever = FileNameRetriever(True)
-            
-            q1_expected_filename= self.get_expected_q_filename_and_update_config(config, "C1", "1.0", True)
-            q2_expected_filename= self.get_expected_q_filename_and_update_config(config, "C2", "2.0", True)
-            q3_expected_filename= self.get_expected_q_filename_and_update_config(config, "C3", "3.0", True)
-            
-            filenames = file_name_retriever.get_file_names(config)
-            
-            assert filenames[(1.0,"C1-TideBC")] == q1_expected_filename
-            assert filenames[(2.0,"C2-TideBC")] == q2_expected_filename
-            assert filenames[(3.0,"C3-TideBC")] == q3_expected_filename
-            
-        def get_expected_q_filename_and_update_config(self, config : ConfigParser, q : str, discharge_value, tide = False):
-            reference = "Reference"
-            with_measure = "WithMeasure"
-            discharge = "Discharge"
-            
-            q_reference_filename = q + "-" + reference
-            q_with_measure_filename = q + "-" + with_measure
-            
-            config.add_section(q)
-            config.set(q, reference, q_reference_filename)
-            config.set(q, with_measure, q_with_measure_filename)
-            config.set(q, discharge, discharge_value)
-            
-            if tide:
-                tide = "TideBC"
-                q_tide = q + "-" + tide
-                config.set(q, tide, q_tide)     
-            
-            return (q_reference_filename,q_with_measure_filename)
+        q1_expected_filename= self.get_expected_q_filename_and_update_config(config, "C1", "1.0", True)
+        q2_expected_filename= self.get_expected_q_filename_and_update_config(config, "C2", "2.0", True)
+        q3_expected_filename= self.get_expected_q_filename_and_update_config(config, "C3", "3.0", True)
+        
+        filenames = file_name_retriever.get_file_names(config)
+        
+        assert filenames[(1.0,"C1-TideBC")] == q1_expected_filename
+        assert filenames[(2.0,"C2-TideBC")] == q2_expected_filename
+        assert filenames[(3.0,"C3-TideBC")] == q3_expected_filename
+        
+    def get_expected_q_filename_and_update_config(self, config : ConfigParser, q : str, discharge_value, tide = False):
+        reference = "Reference"
+        with_measure = "WithMeasure"
+        discharge = "Discharge"
+        
+        q_reference_filename = q + "-" + reference
+        q_with_measure_filename = q + "-" + with_measure
+        
+        config.add_section(q)
+        config.set(q, reference, q_reference_filename)
+        config.set(q, with_measure, q_with_measure_filename)
+        config.set(q, discharge, discharge_value)
+        
+        if tide:
+            tide = "TideBC"
+            q_tide = q + "-" + tide
+            config.set(q, tide, q_tide)     
+        
+        return (q_reference_filename,q_with_measure_filename)
