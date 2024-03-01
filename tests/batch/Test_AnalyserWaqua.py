121ab6834e55b331581efb324ee041466442c48a
Marlon Vermeulen <Marlon.Vermeulen@deltares.nl>
2024-03-01 10:04:18 +0100

    DFAST-122 move test classes to seperate files

--- tests/batch/Test_AnalyserWaqua.py
+++ tests/batch/Test_AnalyserWaqua.py
@@ -1,37 +1,9 @@
 from unittest.mock import patch
-import pytest
 import numpy
-from dfastmi.batch.ReporterWaqua import ReporterWaqua
+import pytest
 from dfastmi.batch.AnalyserWaqua import AnalyserWaqua
-from dfastmi.batch.OutputDataWaqua import OutputDataWaqua
-        
-class Test_ReporterWaqua():
-    def given_output_data_and_mocked_write_files_when_write_report_then_expect_3_calls_for_writing_report(self):
-        """
-        given : output data and mocked write files
-        when :  write report  
-        then  : expect 3 calls for writing report
-        """
-        reporter = ReporterWaqua("filepath")
-        first_min_velocity_m = 0
-        first_min_velocity_n = 0
-        data_zgem = numpy.array([1, 2, 3, 4, 5])
-        data_zmax = numpy.array([1, 2, 3, 4, 5])
-        data_zmin = numpy.array([1, 2, 3, 4, 5])
-        output_data = OutputDataWaqua(first_min_velocity_m, first_min_velocity_n, data_zgem, data_zmax, data_zmin)
-        with patch('dfastmi.batch.ReporterWaqua.ApplicationSettingsHelper.get_filename') as mocked_get_file_name,\
-        patch('dfastmi.batch.ReporterWaqua.DataTextFileOperations.write_simona_box') as mocked_write_simona_box:
-            
-            mocked_get_file_name.return_value = "filename.file"
-            mocked_get_file_name.side_effect = "file.file"
-    
-            reporter.write_report(output_data)
-            
-            assert mocked_write_simona_box.call_count == 3
-            assert mocked_get_file_name.call_count == 3
 
 class Test_AnalyserWaqua():
-    
     @pytest.mark.parametrize("display, reduced_output, old_zmin_zmax", [
         (False, False, False),
         (True, False, False),
@@ -50,7 +22,7 @@ class Test_AnalyserWaqua():
         """
         fraction_of_year = [1.0, 2.0, 3.0]
         rsigma = [0.1, 0.2, 0.3]
-        
+
         tstag = 0.0
         discharges = [1.0, 2.0, 3.0]
         apply_q = [True, True, True]
@@ -62,13 +34,13 @@ class Test_AnalyserWaqua():
         patch('dfastmi.batch.AnalyserWaqua.ApplicationSettingsHelper.get_text') as mocked_get_text, \
         patch('dfastmi.batch.AnalyserWaqua.DataTextFileOperations.read_waqua_xyz') as mocked_read_waqua_xyz, \
         patch('dfastmi.batch.AnalyserWaqua.dfastmi.kernel.core.dzq_from_du_and_h') as mocked_kernel_core:
-            
+
             mocked_get_text.return_value="description"
             mocked_read_waqua_xyz.return_value=numpy.array([1, 2, 3, 4, 5])
             mocked_kernel_core.return_value=numpy.array([1, 2, 3, 4, 5])
 
             output_data = waqua.analyse(fraction_of_year, rsigma)
-            
+
             assert mocked_kernel_core.call_count == 0
 
         assert len(output_data.data_zgem) == 0
@@ -76,8 +48,8 @@ class Test_AnalyserWaqua():
         assert len(output_data.data_zmin) == 0
         assert output_data.first_min_velocity_m == 0
         assert output_data.first_min_velocity_n == 0
-        
-        
+
+
     @pytest.mark.parametrize("display, reduced_output, old_zmin_zmax", [
         (False, False, False),
         (True, False, False),
@@ -96,7 +68,7 @@ class Test_AnalyserWaqua():
         """
         fraction_of_year = [1.0, 2.0, 3.0]
         rsigma = [0.1, 0.2, 0.3]
-        
+
         tstag = 0.0
         discharges = [1.0, 2.0, 3.0]
         apply_q = [True, True, True]
@@ -109,11 +81,11 @@ class Test_AnalyserWaqua():
         patch('dfastmi.batch.AnalyserWaqua.DataTextFileOperations.read_waqua_xyz') as mocked_read_waqua_xyz, \
         patch('dfastmi.batch.AnalyserWaqua.dfastmi.kernel.core.dzq_from_du_and_h') as mocked_kernel_core, \
         patch('dfastmi.batch.AnalyserWaqua.os.path.isfile') as mocked_isfile:
-            
+
             self.setup_mocks(mocked_get_text, mocked_read_waqua_xyz, mocked_isfile)
 
             output_data = waqua.analyse(fraction_of_year, rsigma)
-            
+
             assert mocked_kernel_core.call_count == 3
 
         assert len(output_data.data_zgem) == 0
@@ -121,7 +93,7 @@ class Test_AnalyserWaqua():
         assert len(output_data.data_zmin) == 0
         assert output_data.first_min_velocity_m == 0
         assert output_data.first_min_velocity_n == 0
-        
+
     @pytest.mark.parametrize("display, reduced_output, old_zmin_zmax", [
         (False, False, False),
         (True, False, False),
@@ -140,7 +112,7 @@ class Test_AnalyserWaqua():
         """
         fraction_of_year = [1.0, 2.0, 3.0]
         rsigma = [0.1, 0.2, 0.3]
-        
+
         tstag = 1.0
         discharges = [1.0, 2.0, 3.0]
         apply_q = [True, True, True]
@@ -153,11 +125,11 @@ class Test_AnalyserWaqua():
             patch('dfastmi.batch.AnalyserWaqua.DataTextFileOperations.read_waqua_xyz') as mocked_read_waqua_xyz, \
             patch('dfastmi.batch.AnalyserWaqua.dfastmi.kernel.core.dzq_from_du_and_h') as mocked_kernel_core, \
             patch('dfastmi.batch.AnalyserWaqua.os.path.isfile') as mocked_isfile:
-                
+
                 self.setup_mocks(mocked_get_text, mocked_read_waqua_xyz, mocked_isfile)
 
                 output_data = waqua.analyse(fraction_of_year, rsigma)
-                
+
                 assert mocked_kernel_core.call_count == 3
 
         assert len(output_data.data_zgem) == 0
@@ -165,11 +137,11 @@ class Test_AnalyserWaqua():
         assert len(output_data.data_zmin) == 0
         assert output_data.first_min_velocity_m == 0
         assert output_data.first_min_velocity_n == 0
-        
+
     def setup_mocks(self, mocked_get_text, mocked_read_waqua_xyz, mocked_isfile):
         mocked_isfile.return_value = True
         mocked_get_text.return_value = "description"
-            
+
         def custom_side_effect(*args, **kwargs):
             if "xyz_velocity-zeta.001." in args[0]:
                 return numpy.array([[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]])
