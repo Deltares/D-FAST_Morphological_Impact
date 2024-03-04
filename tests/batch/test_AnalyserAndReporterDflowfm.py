3ebb58e2d417c0dd3bbb71bf7b8507c8c180ba52
Marlon Vermeulen <Marlon.Vermeulen@deltares.nl>
2024-03-04 10:26:06 +0100

    DFAST-123 add test with data

--- tests/batch/test_AnalyserAndReporterDflowfm.py
+++ tests/batch/test_AnalyserAndReporterDflowfm.py
@@ -1,6 +1,9 @@
+from ast import List
 import os
+import random
 from typing import Tuple
 from unittest.mock import patch
+from matplotlib import pyplot as plt
 import pytest
 from dfastmi.batch import AnalyserAndReporterDflowfm
 from dfastmi.io.ApplicationSettingsHelper import ApplicationSettingsHelper
@@ -71,7 +74,7 @@ class Test_analyse_and_report_dflowfm_mode():
         (False, False, True),
         (True, False, True),
     ])   
-    def given_file_names_with_plotting_off_when_analyse_and_report_dflowfm_then_expect_return_true_and_elven_grids_added(self, tmp_path, display : bool, needs_tide : bool, old_zmin_zmax : bool):
+    def given_file_names_with_plotting_off_when_analyse_and_report_dflowfm_then_expect_return_true_and_eleven_grids_added(self, tmp_path, display : bool, needs_tide : bool, old_zmin_zmax : bool):
         report = None
         q_threshold = 1.0
         slength = 1.0
@@ -126,4 +129,91 @@ class Test_analyse_and_report_dflowfm_mode():
                 os.chdir(cwd)
         
         assert succes
+        assert mocked_ugrid_add.call_count == 11
+        
+    @pytest.mark.parametrize("display, needs_tide, old_zmin_zmax", [
+        (False, False, False),
+        (True, False, False),
+        (True, True, False),
+        (True, True, True),
+        (False, True, False),
+        (False, True, True),
+        (False, False, True),
+        (True, False, True),
+    ])
+    def given_file_names_with_plotting_on_when_analyse_and_report_dflowfm_then_expect_return_true_and_eleven_grids_added(self, tmp_path, display : bool, needs_tide : bool, old_zmin_zmax : bool):
+        report = None
+        q_threshold = 1.0
+        slength = 1.0
+        nwidth = 1.0
+        filenames = {}
+        filenames[0] = ("measure-Q1_map.nc", "measure-Q1_map.nc")
+        filenames[1] = ("measure-Q2_map.nc", "measure-Q2_map.nc")
+        filenames[2] = ("measure-Q3_map.nc", "measure-Q3_map.nc")
+        xykm = None
+        n_fields = 1
+        tide_bc: Tuple[str, ...] = ("name1", "name2")
+        kmbounds : Tuple[float, float] = (1.0, 2.0)
+        plotops = {}
+        plotops['plotting'] = True
+        plotops['saveplot'] = True
+        plotops['saveplot_zoomed'] = True
+        plotops['figdir'] = str(tmp_path)
+        plotops['plot_ext'] = "plot_ext"
+        
+        random_list: List[Tuple[float, float, float, float]] = [
+            (random.uniform(0.0, 100.0), random.uniform(0.0, 100.0), random.uniform(0.0, 100.0), random.uniform(0.0, 100.0))]
+        
+        plotops['xyzoom'] = random_list
+        
+        tstag = 1.0
+        Q = [1.0, 2.0, 3.0]
+        T = [1.0, 2.0, 3.0]
+        rsigma = [0.1, 0.2, 0.3]
+        ucrit = 0.3
+        outputdir = str(tmp_path)
+        
+        with patch('dfastmi.batch.AnalyserAndReporterDflowfm.GridOperations.ugrid_add') as mocked_ugrid_add, \
+             patch('dfastmi.batch.AnalyserAndReporterDflowfm.dfastmi.plotting.plot_overview') as mocked_plotting_plot_overview, \
+             patch('dfastmi.batch.AnalyserAndReporterDflowfm.dfastmi.plotting.zoom_xy_and_save') as mocked_plotting_zoom_xy_and_save, \
+             patch('dfastmi.batch.AnalyserAndReporterDflowfm.dfastmi.plotting.savefig') as mocked_plotting_savefig:
+            
+            Figure = plt.figure
+            Axes = plt.axes
+            mocked_plotting_plot_overview.return_value = ((Figure(figsize=(8, 6)) ,Axes()))
+            
+            ApplicationSettingsHelper.load_program_texts("dfastmi/messages.NL.ini")
+
+            cwd = os.getcwd()
+            tstdir = "tests/c01 - GendtseWaardNevengeul"
+            try:
+                os.chdir(tstdir)
+                succes = AnalyserAndReporterDflowfm.analyse_and_report_dflowfm(
+                    display,
+                    report,
+                    q_threshold,
+                    tstag,
+                    Q,
+                    T,
+                    rsigma,
+                    slength,
+                    nwidth,
+                    ucrit,
+                    filenames,
+                    xykm,
+                    needs_tide,
+                    n_fields,
+                    tide_bc,
+                    old_zmin_zmax,
+                    kmbounds,
+                    outputdir,
+                    plotops)
+            finally:
+                os.chdir(cwd)
+        
+        assert succes
+        assert mocked_plotting_plot_overview.call_count == 1
+        assert mocked_plotting_zoom_xy_and_save.call_count == 1
+        assert mocked_plotting_savefig.call_count == 1
+        assert mocked_ugrid_add.call_count == 11
         assert mocked_ugrid_add.call_count == 11
