be64267aeee31db5cdad2d362d09f50d71cc4dd2
Marlon Vermeulen <Marlon.Vermeulen@deltares.nl>
2024-03-05 09:02:04 +0100

    DFAST-123 add test with data

--- tests/batch/test_AnalyserAndReporterDflowfm.py
+++ tests/batch/test_AnalyserAndReporterDflowfm.py
@@ -65,6 +65,62 @@ class Test_analyse_and_report_dflowfm_mode():
 
         assert succes
         
+    @pytest.mark.parametrize("display, needs_tide, old_zmin_zmax", [
+        (False, False, False),
+        (True, False, False),
+        (True, True, False),
+        (True, True, True),
+        (False, True, False),
+        (False, True, True),
+        (False, False, True),
+        (True, False, True),
+    ])   
+    def given_file_names_based_on_string_when_analyse_and_report_dflowfm_then_return_true(self, tmp_path, display : bool, needs_tide : bool, old_zmin_zmax : bool):
+        report = None
+        q_threshold = 1.0
+        slength = 1.0
+        nwidth = 1.0
+        filenames = {}
+        filenames["0"] = ("measure-Q1_map.nc", "measure-Q1_map.nc")
+        filenames["1"] = ("measure-Q2_map.nc", "measure-Q2_map.nc")
+        filenames["2"] = ("measure-Q3_map.nc", "measure-Q3_map.nc")
+        xykm = None
+        n_fields = 3
+        tide_bc: Tuple[str, ...] = ("name1", "name2")
+        kmbounds : Tuple[float, float] = (1.0, 2.0)
+        plotops = {}
+        
+        tstag = 1.0
+        Q = [1.0, 2.0, 3.0]
+        T = [1.0, 2.0, 3.0]
+        rsigma = [0.1, 0.2, 0.3]
+        ucrit = 0.3
+        outputdir = str(tmp_path)
+        
+
+        succes = AnalyserAndReporterDflowfm.analyse_and_report_dflowfm(
+                display,
+                report,
+                q_threshold,
+                tstag,
+                Q,
+                T,
+                rsigma,
+                slength,
+                nwidth,
+                ucrit,
+                filenames,
+                xykm,
+                needs_tide,
+                n_fields,
+                tide_bc,
+                old_zmin_zmax,
+                kmbounds,
+                outputdir,
+                plotops)
+
+        assert succes
+        
         
     @pytest.mark.parametrize("display, needs_tide, old_zmin_zmax", [
         (False, False, False),
