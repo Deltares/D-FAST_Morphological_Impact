9e3e321a9fcbb88489ae95ed724884750faf2d8a
ralph-peelen-deltares <ralph.peelen@deltares.nl>
2024-03-06 12:10:03 +0100

    DFAST-120: renamed test for configuration checker factory with more clear names

--- tests/batch/Test_configuration_checker_factory.py
+++ tests/batch/Test_configuration_checker_factory.py
@@ -8,23 +8,23 @@ from packaging import version
 
 
 class Test_configuration_checker_factory():
-    def given_legacy_when_then(self):
+    def given_legacy_configuration_version_when_generating_ConfigurationChecker_then_validate_generated_ConfigurationChecker_is_a_ConfigurationCheckerLegacy(self):
         legacy_version = version.parse("1.0")
         legacy_configuration_checker = ConfigurationCheckerFactory.generate(legacy_version)
         assert isinstance(legacy_configuration_checker, ConfigurationCheckerLegacy)
     
-    def given_when_then(self):
+    def given_valid_configuration_version_when_generating_ConfigurationChecker_then_validate_generated_ConfigurationChecker_is_a_ConfigurationChecker(self):
         correct_version = version.parse("2.0")
         legacy_configuration_checker = ConfigurationCheckerFactory.generate(correct_version)
         assert isinstance(legacy_configuration_checker, ConfigurationChecker)
     
-    def given_unknown_when_then(self):
+    def given_invalid_configuration_version_when_generating_ConfigurationChecker_then_throw_exception_unknown_version(self):
         unknown_version = version.parse("0.0")
         with pytest.raises(Exception) as cm:
             ConfigurationCheckerFactory.generate(unknown_version)
         assert str(cm.value) == f"No ConfigurationChecker constructor registered for version {unknown_version}"
         
-    def given_None_when_then(self):
+    def given_no_configuration_version_when_generating_ConfigurationChecker_then_throw_exception_unknown_version(self):
         with pytest.raises(Exception) as cm:
             ConfigurationCheckerFactory.generate(None)
         assert str(cm.value) == f"No ConfigurationChecker constructor registered for version {None}"
