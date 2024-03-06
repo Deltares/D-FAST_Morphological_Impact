502290b8412e8832be095b94d254f45318a4e125
Marlon Vermeulen <Marlon.Vermeulen@deltares.nl>
2024-03-06 14:19:13 +0100

    DFAST-123 resolve tests after update from main

--- tests/batch/test_AnalyserAndReporterDflowfm.py
+++ tests/batch/test_AnalyserAndReporterDflowfm.py
@@ -136,22 +136,20 @@ class Test_analyse_and_report_dflowfm_mode():
         assert succes
 
         
-    @pytest.mark.parametrize("display, needs_tide, old_zmin_zmax", [
-        (False, False, False),
-        (True, False, False),
-        (True, True, False),
-        (True, True, True),
-        (False, True, False),
-        (False, True, True),
-        (False, False, True),
-        (True, False, True),
+    @pytest.mark.parametrize("display, old_zmin_zmax", [
+        (False, False),
+        (True,False),
+        (True,True),
+        (False, True),
     ])   
-    def given_file_names_based_on_numbers_with_plotting_off_when_analyse_and_report_dflowfm_then_return_true_and_expect_eleven_grids_added_and_plotting_not_called(self, tmp_path, display : bool, needs_tide : bool, old_zmin_zmax : bool , setup):        
+    def given_file_names_based_on_numbers_with_plotting_off_and_needs_tide_false_when_analyse_and_report_dflowfm_then_return_true_and_expect_eleven_grids_added_and_plotting_not_called(self, tmp_path, display : bool, old_zmin_zmax : bool , setup):        
         """
-        given : file names based on numbers with plotting off
+        given : file names based on numbers with plotting off and needs tide false
         when  : analyse and report dflowfm
         then  : return true and expect eleven grids added and plotting not called
         """
+        needs_tide = False
+        
         outputdir = str(tmp_path)
         
         self.n_fields = 1
@@ -205,23 +203,89 @@ class Test_analyse_and_report_dflowfm_mode():
         assert mocked_plotting_zoom_xy_and_save.call_count == 0
         assert mocked_plotting_savefig.call_count == 0
         assert mocked_ugrid_add.call_count == 11
-        
-    @pytest.mark.parametrize("display, needs_tide, old_zmin_zmax", [
-        (False, False, False),
-        (True, False, False),
-        (True, True, False),
-        (True, True, True),
-        (False, True, False),
-        (False, True, True),
-        (False, False, True),
-        (True, False, True),
-    ])
-    def given_file_names_with_plotting_on_when_analyse_and_report_dflowfm_then_return_true_and_expect_eleven_grids_added_and_plotting_called(self, tmp_path, display : bool, needs_tide : bool, old_zmin_zmax : bool, setup):        
+
+    @pytest.mark.parametrize("display, old_zmin_zmax", [
+        (False, False),
+        (True,False),
+        (True,True),
+        (False, True),
+    ])   
+    def given_file_names_based_on_numbers_with_plotting_off_and_needs_tide_true_when_analyse_and_report_dflowfm_then_return_true_and_expect_zero_grids_added_and_plotting_not_called(self, tmp_path, display : bool, old_zmin_zmax : bool , setup):        
         """
-        given : file names based on numbers with plotting on
+        given : file names based on numbers with plotting off and needs tide true
+        when  : analyse and report dflowfm
+        then  : return true and expect zero grids added and plotting not called
+        """
+        needs_tide = True
+        
+        outputdir = str(tmp_path)
+        
+        self.n_fields = 1
+        
+        self.plotops['plotting'] = False
+        
+        self.filenames[0] = ("measure-Q1_map.nc", "measure-Q1_map.nc")
+        self.filenames[1] = ("measure-Q2_map.nc", "measure-Q2_map.nc")
+        self.filenames[2] = ("measure-Q3_map.nc", "measure-Q3_map.nc")
+        
+        with patch('dfastmi.batch.AnalyserAndReporterDflowfm.GridOperations.ugrid_add') as mocked_ugrid_add, \
+             patch('dfastmi.batch.AnalyserAndReporterDflowfm.dfastmi.plotting.plot_overview') as mocked_plotting_plot_overview, \
+             patch('dfastmi.batch.AnalyserAndReporterDflowfm.dfastmi.plotting.zoom_xy_and_save') as mocked_plotting_zoom_xy_and_save, \
+             patch('dfastmi.batch.AnalyserAndReporterDflowfm.dfastmi.plotting.savefig') as mocked_plotting_savefig, \
+             patch('dfastmi.batch.AnalyserAndReporterDflowfm.comp_sedimentation_volume') as mocked_comp_sedimentation_volume:
+
+            mocked_plotting_plot_overview.return_value = ((plt.figure(figsize=(8, 6)) ,plt.axes()))
+            mocked_comp_sedimentation_volume.return_value = (None, None, [], None, None, [], numpy.zeros(2555), numpy.zeros(2555))
+            
+            ApplicationSettingsHelper.load_program_texts("dfastmi/messages.NL.ini")
+
+            cwd = os.getcwd()
+            tstdir = "tests/c01 - GendtseWaardNevengeul"
+            try:
+                os.chdir(tstdir)
+                succes = AnalyserAndReporterDflowfm.analyse_and_report_dflowfm(
+                    display,
+                    self.report,
+                    self.q_threshold,
+                    self.tstag,
+                    self.Q,
+                    self.T,
+                    self.rsigma,
+                    self.slength,
+                    self.nwidth,
+                    self.ucrit,
+                    self.filenames,
+                    self.xykm,
+                    needs_tide,
+                    self.n_fields,
+                    self.tide_bc,
+                    old_zmin_zmax,
+                    self.kmbounds,
+                    outputdir,
+                    self.plotops)
+            finally:
+                os.chdir(cwd)
+        
+        assert succes
+        assert mocked_plotting_plot_overview.call_count == 0
+        assert mocked_plotting_zoom_xy_and_save.call_count == 0
+        assert mocked_plotting_savefig.call_count == 0
+        assert mocked_ugrid_add.call_count == 0
+        
+    @pytest.mark.parametrize("display, old_zmin_zmax", [
+        (False, False),
+        (True,False),
+        (True,True),
+        (False, True),
+    ])
+    def given_file_names_with_plotting_on_and_needs_tide_false_when_analyse_and_report_dflowfm_then_return_true_and_expect_eleven_grids_added_and_plotting_called(self, tmp_path, display : bool, old_zmin_zmax : bool, setup):        
+        """
+        given : file names with plotting on and needs tide false
         when  : analyse and report dflowfm
         then  : return true and expect eleven grids added and plotting called
         """
+        needs_tide = False
+        
         outputdir = str(tmp_path)
         
         self.n_fields = 1
@@ -285,6 +349,83 @@ class Test_analyse_and_report_dflowfm_mode():
         assert mocked_plotting_savefig.call_count == 1
         assert mocked_ugrid_add.call_count == 11
         
+    @pytest.mark.parametrize("display, old_zmin_zmax", [
+        (False, False),
+        (True,False),
+        (True,True),
+        (False, True),
+    ])
+    def given_file_names_with_plotting_on_and_needs_tide_true_when_analyse_and_report_dflowfm_then_return_true_and_expect_zero_grids_added_and_plotting_not_called(self, tmp_path, display : bool, old_zmin_zmax : bool, setup):        
+        """
+        given : file names with plotting on and needs tide false
+        when  : analyse and report dflowfm
+        then  : return true and expect zero grids added and plotting not called
+        """
+        needs_tide = True
+        
+        outputdir = str(tmp_path)
+        
+        self.n_fields = 1
+        
+        self.plotops['plotting'] = True
+        self.plotops['saveplot'] = True
+        self.plotops['saveplot_zoomed'] = True
+        self.plotops['figdir'] = str(tmp_path)
+        self.plotops['plot_ext'] = "plot_ext"
+        
+        random_list: List[Tuple[float, float, float, float]] = [
+            (random.uniform(0.0, 100.0), random.uniform(0.0, 100.0), random.uniform(0.0, 100.0), random.uniform(0.0, 100.0))]
+        
+        self.plotops['xyzoom'] = random_list
+        
+        self.filenames[0] = ("measure-Q1_map.nc", "measure-Q1_map.nc")
+        self.filenames[1] = ("measure-Q2_map.nc", "measure-Q2_map.nc")
+        self.filenames[2] = ("measure-Q3_map.nc", "measure-Q3_map.nc")
+        
+        with patch('dfastmi.batch.AnalyserAndReporterDflowfm.GridOperations.ugrid_add') as mocked_ugrid_add, \
+             patch('dfastmi.batch.AnalyserAndReporterDflowfm.dfastmi.plotting.plot_overview') as mocked_plotting_plot_overview, \
+             patch('dfastmi.batch.AnalyserAndReporterDflowfm.dfastmi.plotting.zoom_xy_and_save') as mocked_plotting_zoom_xy_and_save, \
+             patch('dfastmi.batch.AnalyserAndReporterDflowfm.dfastmi.plotting.savefig') as mocked_plotting_savefig, \
+             patch('dfastmi.batch.AnalyserAndReporterDflowfm.comp_sedimentation_volume') as mocked_comp_sedimentation_volume:
+
+            mocked_plotting_plot_overview.return_value = ((plt.figure(figsize=(8, 6)) ,plt.axes()))
+            mocked_comp_sedimentation_volume.return_value = (None, None, [], None, None, [], numpy.zeros(2555), numpy.zeros(2555))
+            
+            ApplicationSettingsHelper.load_program_texts("dfastmi/messages.NL.ini")
+
+            cwd = os.getcwd()
+            tstdir = "tests/c01 - GendtseWaardNevengeul"
+            try:
+                os.chdir(tstdir)
+                succes = AnalyserAndReporterDflowfm.analyse_and_report_dflowfm(
+                    display,
+                    self.report,
+                    self.q_threshold,
+                    self.tstag,
+                    self.Q,
+                    self.T,
+                    self.rsigma,
+                    self.slength,
+                    self.nwidth,
+                    self.ucrit,
+                    self.filenames,
+                    self.xykm,
+                    needs_tide,
+                    self.n_fields,
+                    self.tide_bc,
+                    old_zmin_zmax,
+                    self.kmbounds,
+                    outputdir,
+                    self.plotops)
+            finally:
+                os.chdir(cwd)
+        
+        assert succes
+        assert mocked_plotting_plot_overview.call_count == 0
+        assert mocked_plotting_zoom_xy_and_save.call_count == 0
+        assert mocked_plotting_savefig.call_count == 0
+        assert mocked_ugrid_add.call_count == 0
+        
         
     @pytest.mark.parametrize("needs_tide, old_zmin_zmax", [
         (False, False),
@@ -324,7 +465,7 @@ class Test_analyse_and_report_dflowfm_mode():
              patch('dfastmi.batch.AnalyserAndReporterDflowfm.comp_sedimentation_volume') as mocked_comp_sedimentation_volume:
 
             mocked_plotting_plot_overview.return_value = ((plt.figure(figsize=(8, 6)) ,plt.axes()))
-            mocked_comp_sedimentation_volume.return_value = (None, None, [], None, None, [], numpy.zeros(2555), numpy.zeros(2555))
+            mocked_comp_sedimentation_volume.return_value = (None, None, [], None, None, [], numpy.zeros(0), numpy.zeros(0))
 
             ApplicationSettingsHelper.load_program_texts("dfastmi/messages.NL.ini")
 
