0f0dbf8d172b03e245a8df02a9fa681041135cb1
ralph-peelen-deltares <ralph.peelen@deltares.nl>
2024-03-19 07:35:18 +0100

    DFAST-110: Improve get program location test. Check if the folder ends with dfastmi
    Co-authored-by: Prisca van der Sluis <36264671+priscavdsluis@users.noreply.github.com>

--- tests/io/test_DFastUtils.py
+++ tests/io/test_DFastUtils.py
@@ -1,10 +1,9 @@
 
 from dfastmi.batch.DFastUtils import get_progloc
 
-class Test_DFastUtils():
-    class Test_get_progloc():
-        def test_get_program_location(self):
-            """
-            Get the location of the program.
-            """
-            assert get_progloc() != ""
+class TestGetProgloc():
+    def test_get_program_location(self):
+        """
+        Get the location of the program.
+        """
+        assert get_progloc().endswith("dfastmi")
