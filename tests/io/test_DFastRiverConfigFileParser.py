a9b101589d066a7a90358dc0b5cf5e413211f8d1
Tim van den Aardweg <tim.vandenaardweg@deltares.nl>
2024-04-11 10:55:24 +0200

    DFAST-145: Added testcases for getstring.

--- tests/io/test_DFastRiverConfigFileParser.py
+++ tests/io/test_DFastRiverConfigFileParser.py
@@ -262,6 +262,82 @@ class TestDFastRiverConfigFileParser:
         assert isinstance(value, bool)
         assert value == fallback
 
+    def test_getstring_key_only_in_general_section_returns_value_from_general_section(
+        self,
+    ):
+        # setup
+        reach = self._get_reach()
+        config = self._get_config_parser()
+        parser = DFastRiverConfigFileParser(config)
+
+        # call
+        value = parser.getstring("general_str_key", reach)
+
+        # assert
+        assert isinstance(value, str)
+        expected_value = "abc"
+        assert value == expected_value
+
+    def test_getstring_key_both_in_general_section_and_in_branch_section_returns_value_from_branch_section(
+        self,
+    ):
+        # setup
+        reach = self._get_reach(1, "Branch1")
+        config = self._get_config_parser()
+        parser = DFastRiverConfigFileParser(config)
+
+        # call
+        value = parser.getstring("str_key_in_general_and_branch_section", reach)
+
+        # assert
+        assert isinstance(value, str)
+        expected_value = "def"
+        assert value == expected_value
+
+    def test_getstring_key_in_general_section_and_in_branch_section_and_in_reach_returns_value_from_reach(
+        self,
+    ):
+        # setup
+        reach = self._get_reach(1, "Branch1")
+        config = self._get_config_parser()
+        parser = DFastRiverConfigFileParser(config)
+
+        # call
+        value = parser.getstring("str_key_in_general_and_branch_and_reach", reach)
+
+        # assert
+        assert isinstance(value, str)
+        expected_value = "ghi"
+        assert value == expected_value
+
+    def test_getstring_key_not_found_and_no_fallback_given_returns_default_fallback(self):
+        # setup
+        reach = self._get_reach(1, "Branch1")
+        config = self._get_config_parser()
+        parser = DFastRiverConfigFileParser(config)
+
+        # call
+        value = parser.getstring("This key does not exist", reach)
+
+        # assert
+        assert isinstance(value, str)
+        default_fallback = ""
+        assert value == default_fallback
+
+    def test_getstring_key_not_found_and_fallback_given_returns_fallback(self):
+        # setup
+        reach = self._get_reach(1, "Branch1")
+        config = self._get_config_parser()
+        parser = DFastRiverConfigFileParser(config)
+
+        # call
+        fallback = "abcdef"
+        value = parser.getstring("This key does not exist", reach, fallback)
+
+        # assert
+        assert isinstance(value, str)
+        assert value == fallback
+
     @staticmethod
     def _get_reach(
         reach_index: int = 1, branch_name: str = "randomBranchName"
@@ -286,15 +362,19 @@ class TestDFastRiverConfigFileParser:
         [General]
         general_int_key = 123   
         int_key_in_general_and_branch_section = 123
-        int_key_in_general_and_branch_and_reach1 = 123
+        int_key_in_general_and_branch_and_reach = 123
         
         general_float_key = 123.123   
         float_key_in_general_and_branch_section = 123.123
-        float_key_in_general_and_branch_and_reach1 = 123.123
+        float_key_in_general_and_branch_and_reach = 123.123
         
         general_bool_key = True
         bool_key_in_general_and_branch_section = False
-        bool_key_in_general_and_branch_and_reach1 = True
+        bool_key_in_general_and_branch_and_reach = True
+        
+        general_str_key = abc
+        str_key_in_general_and_branch_section = abc
+        str_key_in_general_and_branch_and_reach = abc
         
         [Branch1]
         int_key_in_general_and_branch_section = 456
@@ -303,8 +383,11 @@ class TestDFastRiverConfigFileParser:
         float_key_in_general_and_branch_and_reach = 456.456
         bool_key_in_general_and_branch_section = True
         bool_key_in_general_and_branch_and_reach = False
+        str_key_in_general_and_branch_section = def
+        str_key_in_general_and_branch_and_reach = def
         
         int_key_in_general_and_branch_and_reach1 = 789
         float_key_in_general_and_branch_and_reach1 = 789.789
         bool_key_in_general_and_branch_and_reach1 = True
+        str_key_in_general_and_branch_and_reach1 = ghi
         """
