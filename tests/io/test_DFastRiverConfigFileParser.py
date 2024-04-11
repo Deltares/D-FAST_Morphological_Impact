ab73604f28585d7730657d78364b1f8af77a3dd2
Tim van den Aardweg <tim.vandenaardweg@deltares.nl>
2024-04-11 10:39:58 +0200

    DFAST-145: Added testcases for getfloat.

--- tests/io/test_DFastRiverConfigFileParser.py
+++ tests/io/test_DFastRiverConfigFileParser.py
@@ -110,6 +110,82 @@ class TestDFastRiverConfigFileParser:
         assert isinstance(value, int)
         assert value == fallback
 
+    def test_getfloat_key_only_in_general_section_returns_value_from_general_section(
+        self,
+    ):
+        # setup
+        reach = self._get_reach()
+        config = self._get_config_parser()
+        parser = DFastRiverConfigFileParser(config)
+
+        # call
+        value = parser.getfloat("general_float_key", reach)
+
+        # assert
+        assert isinstance(value, float)
+        expected_value = 123.123
+        assert value == expected_value
+
+    def test_getfloat_key_both_in_general_section_and_in_branch_section_returns_value_from_branch_section(
+        self,
+    ):
+        # setup
+        reach = self._get_reach(1, "Branch1")
+        config = self._get_config_parser()
+        parser = DFastRiverConfigFileParser(config)
+
+        # call
+        value = parser.getfloat("float_key_in_general_and_branch_section", reach)
+
+        # assert
+        assert isinstance(value, float)
+        expected_value = 456.456
+        assert value == expected_value
+
+    def test_getfloat_key_in_general_section_and_in_branch_section_and_in_reach_returns_value_from_reach(
+        self,
+    ):
+        # setup
+        reach = self._get_reach(1, "Branch1")
+        config = self._get_config_parser()
+        parser = DFastRiverConfigFileParser(config)
+
+        # call
+        value = parser.getfloat("float_key_in_general_and_branch_and_reach", reach)
+
+        # assert
+        assert isinstance(value, float)
+        expected_value = 789.789
+        assert value == expected_value
+
+    def test_getfloat_key_not_found_and_no_fallback_given_returns_default_fallback(self):
+        # setup
+        reach = self._get_reach(1, "Branch1")
+        config = self._get_config_parser()
+        parser = DFastRiverConfigFileParser(config)
+
+        # call
+        value = parser.getfloat("This key does not exist", reach)
+
+        # assert
+        assert isinstance(value, float)
+        default_fallback = 0
+        assert value == default_fallback
+
+    def test_getfloat_key_not_found_and_fallback_given_returns_fallback(self):
+        # setup
+        reach = self._get_reach(1, "Branch1")
+        config = self._get_config_parser()
+        parser = DFastRiverConfigFileParser(config)
+
+        # call
+        fallback = 123456.123456
+        value = parser.getfloat("This key does not exist", reach, fallback)
+
+        # assert
+        assert isinstance(value, float)
+        assert value == fallback
+
     @staticmethod
     def _get_reach(
         reach_index: int = 1, branch_name: str = "randomBranchName"
@@ -136,9 +212,16 @@ class TestDFastRiverConfigFileParser:
         int_key_in_general_and_branch_section = 123
         int_key_in_general_and_branch_and_reach = 123
         
+        general_float_key = 123.123   
+        float_key_in_general_and_branch_section = 123.123
+        float_key_in_general_and_branch_and_reach = 123.123
+        
         [Branch1]
         int_key_in_general_and_branch_section = 456
         int_key_in_general_and_branch_and_reach = 456
+        float_key_in_general_and_branch_section = 456.456
+        float_key_in_general_and_branch_and_reach = 456.456
         
         int_key_in_general_and_branch_and_reach1 = 789
+        float_key_in_general_and_branch_and_reach1 = 789.789
         """
