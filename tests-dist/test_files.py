28402d83184061983ef79fc29769c2b87fd73805
hrajagers <hrajagers@gmail.com>
2024-06-10 17:52:07 +0300

    Add tests whether various files are actually included in the distribution.

---
+++ tests-dist/test_files.py
@@ -0,0 +1,26 @@
+import pytest
+from pathlib import Path
+
+# dfast binary path relative to root
+dfastdir = "dfastmi.dist/"
+
+class Test_files_included:
+
+    @pytest.mark.parametrize(
+        "filename",
+        [
+            "LICENSE.md",
+            "dfastmi.exe",
+            "dfastmi/dfastmi_usermanual.pdf",
+            "dfastmi/D-FASTMI.png",
+            "dfastmi/open.png",
+            "dfastmi/Dutch_rivers_v1.ini",
+            "dfastmi/Dutch_rivers_v3.ini",
+            "dfastmi/messages.UK.ini",
+        ]
+    )
+    def test_included(self):
+        """
+        Test whether a specific file is included.
+        """
+        assert Path(dfastdir + filename).is_file()
